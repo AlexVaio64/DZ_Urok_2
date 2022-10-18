fd6e7adb74b9d333b640e2a49d137ccca7f2f878
Michel Nekrasov <moscou@inbox.ru>
2022-10-18 23:58:37 +0300

    add Zadacha 13

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+
+// int Number = new Random().Next(1, 1500);
+// System.Console.WriteLine();
+// System.Console.WriteLine($"Дано число: {Number}");
+
+Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine()!);
+if (Number / 100 > 0) 
+{
+    Console.WriteLine($"Число {Number / 100 % 10} является третьей цифрой числа {Number}.");
+}
+
+else 
+{
+    Console.WriteLine($"У числа {Number} нет третьей цифры!");
+}
