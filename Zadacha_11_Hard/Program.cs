004f5e1a54f59af6c56f695860709fd383c44cd3
Michel Nekrasov <moscou@inbox.ru>
2022-10-19 01:21:21 +0300

    add Zadacha 11

---
+++ Zadacha_11_Hard/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа принимает на вход целое число любой разрядности 
+// и удаляет вторую цифру этого числа.
+
+Console.WriteLine("Введите любое целое число любой разрядности: ");
+int Number = int.Parse(Console.ReadLine()!);
+int NewNumber = 0;
+
+    int NumberWithoutSecDig()
+
+    {
+        NewNumber = Number / 100 * 10 + Number % 10;
+        return NewNumber;
+    }
+
+NumberWithoutSecDig();
+Console.WriteLine($"Если из данного числа удалить вторую цифру, получится число {NewNumber}.");
---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+
+Console.WriteLine("Введите цифру от 1 до 7 и программа сообщит, что это за день недели и является ли он выходным: ");
+int Number = int.Parse(Console.ReadLine()!);
+
+if((0 < Number) && (Number < 8))
+{
+    if(Number == 1) Console.WriteLine("Это понедельник, будний день.");
+    if(Number == 2) Console.WriteLine("Это вторник, будний день.");
+    if(Number == 3) Console.WriteLine("Это среда, будний день.");
+    if(Number == 4) Console.WriteLine("Это четверг, будний день.");
+    if(Number == 5) Console.WriteLine("Это пятница, будний день.");
+    if(Number == 6) Console.WriteLine("Ура! Это суббота, выходной день!");
+    if(Number == 7) Console.WriteLine("Ура! Это воскресенье, выходной день!");
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное число!");
+}
